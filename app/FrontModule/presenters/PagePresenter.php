603421903d6c12cf3374bd92808d54a48e166522
mlazovla <mlazovla@gmail.com>
2015-11-20 11:05:16 +0100

    [add] About page and Autors page

---
+++ app/FrontModule/presenters/PagePresenter.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\FrontModule\Presenters;
+
+use Nette,
+	App\Model;
+
+/**
+ * Page presenter.
+ */
+class PagePresenter extends BasePresenter
+{
+	// No renders needed. Only template files are important.
+
+
+};
